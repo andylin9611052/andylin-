ab2917fdb16a8f208a38d9b71086b36069a05133
andylin <andylin9611052@gmail.com>
2021-11-09 14:09:04 +0800

    initial commit

---
+++ src/main/java/com/lin/Stars.java
@@ -0,0 +1,12 @@
+package com.lin;
+
+public class Stars {
+    public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < 20; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
